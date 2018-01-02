00cdb04c442f05a319a07cf6fbbe955a6d70bc29
Daniel Phillips <danieledward.phillips@gmail.com>
2018-01-02 13:43:43 -0700

    Add relationships to models

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,4 @@
 class Contact < ApplicationRecord
+  has_many :user_contacts
+  has_many :users, through: :user_contacts
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  has_many :user_contacts
+  has_many :contacts, through: :user_contacts
+
 end
