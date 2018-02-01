8a2fdd12af38adea408c642c8eb4b56396ebeb44
Daniel Phillips <danieledward.phillips@gmail.com>
2018-01-31 23:52:00 -0700

    ...

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
 
   def create
     code = params["code"]
-    uri = URI("https://www.linkedin.com/oauth/v2/accessToken?grant_type=authorization_code&code=#{code}&client_id=#{ENV["LINKEDIN_CLIENT_ID"]}&client_secret=#{ENV["LINKEDIN_CLIENT_SECRET"]}&redirect_uri=http://localhost:3000/auth/linkedin/callback")
+    uri = URI("https://www.linkedin.com/oauth/v2/accessToken?grant_type=authorization_code&code=#{code}&client_id=#{ENV["LINKEDIN_CLIENT_ID"]}&client_secret=#{ENV["LINKEDIN_CLIENT_SECRET"]}&redirect_uri=https://powerful-stream-11261.herokuapp.com/auth/linkedin/callback")
     resp = Net::HTTP.post_form(uri, {})
     parsed_resp = JSON.parse(resp.body)
 
